e57e6c2f512411e0c96132aadceff329f9c30377
Ajay <ajaytanpure92@gmail.com>
2017-05-11 23:40:48 -0700

    Bug fix in jobPosting update and email sendig api

--- src/main/java/edu/sjsu/services/JobPostingService.java
+++ src/main/java/edu/sjsu/services/JobPostingService.java
@@ -123,69 +123,59 @@ public class JobPostingService {
 
 		if (parameters.containsKey("jobDescription")) {
 			jobPosting.setJobDescription((String) parameters.get("jobDescription"));
-			if (changed)
-				message.append(", ");
 			changed = true;
-			message.append("job descrption ");
 		}
 		if (parameters.containsKey("responsibilities")) {
 			jobPosting.setResponsibilities((String) parameters.get("responsibilities"));
-			if (changed)
-				message.append(", ");
 			changed = true;
-			message.append("job responsibilities ");
 		}
 		if (parameters.containsKey("location")) {
 			jobPosting.setLocation((String) parameters.get("location"));
-			if (changed)
-				message.append(", ");
 			changed = true;
-			message.append("location");
 		}
 		if (parameters.containsKey("skills")) {
 			String skillInput = (String) parameters.get("skills");
 			List<String> skillsInputList = Arrays.asList(skillInput.split("\\s*,\\s*"));
 			List<Skill> skills = getSkillList(skillsInputList);
 			jobPosting.setSkills(skills);
-			if (changed)
-				message.append(", ");
 			changed = true;
-			message.append("required skills ");
 		}
 		if (parameters.containsKey("status")) {
 			jobPosting.setStatus((Integer) parameters.get("status"));
-			if (changed)
-				message.append(", ");
-			changed = true;
 			statusChanged = (Integer) parameters.get("status");
-			message.append("job status ");
+			jobPosting.setStatus(statusChanged);
 		}
 		if (parameters.containsKey("salary")) {
 			jobPosting.setSalary(Integer.valueOf(String.valueOf(parameters.get("salary"))));
-			if (changed)
-				message.append(", ");
 			changed = true;
-			message.append("salary ");
 		}
 		jobPostingRepository.save(jobPosting);
 
 		String details = null;
 		if (statusChanged == 1)
-			details = "Position has been filled.Thanks for applying";
+			details = "\nFollowing position has been filled.";
 		else if (statusChanged == 2)
-			details = "Position has been Cancelled.Thanks for applying";
+			details = "\nFollowing position has been Cancelled.";
 
 		// Send the email to all the applicant here
-		if (changed) {
+		if (changed || statusChanged > 0) {
 			List<JobApplication> jobApplications;
 			try {
 				jobApplications = jobApplicationService.getJobApplicationByJobPosting(jobPosting);
 				for (JobApplication jobApplication : jobApplications) {
 					JobSeeker applicant = jobApplication.getApplicant();
-					String subject = "Job Id " + jobPosting.getRequisitionId() + " has been updated";
-					message.append("has been updated\n");
+					String subject = "Job - " + jobPosting.getTitle() + " at "
+							+ jobPosting.getCompany().getCompanyName() + " has been updated";
+					message.append("Dear " + applicant.getFirstname());
+					if (changed)
+						message.append(
+								"\n\nFollowing job has been updated.\nPlease have look at updated job details on portal.");
 					if (details != null)
 						message.append(details);
+					message.append("\n\nJob Position : " + jobPosting.getTitle());
+					message.append("\nJob Requisition Id : " + jobPosting.getRequisitionId());
+					message.append(
+							"\n\nBest Regards\nTalent Acquisition Team\n" + jobPosting.getCompany().getCompanyName());
 					emailService.sendMail(applicant.getEmail(), subject, message.toString());
 				}
 			} catch (JobApplicationExceptions e) {
