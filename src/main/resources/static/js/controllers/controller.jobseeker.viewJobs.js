45d47653f998a1b803f86ee5b6f713ef727516ae
Niranjan Tungatkar <niranjananil.tungatkar@sjsu.edu>
2017-05-13 16:47:43 -0700

    Search and pagination integrated with UI

--- src/main/resources/static/js/controllers/controller.jobseeker.viewJobs.js
+++ src/main/resources/static/js/controllers/controller.jobseeker.viewJobs.js
@@ -1,14 +1,36 @@
 jobPortalApp.controller('controllerViewJobs', function($scope, $state, $stateParams, $http, $log) {
 
 
+	$scope.jobSearch = {
+    		freeText:"",
+    		companies:"",
+    		location:"",
+    		minSalary:"",
+    		maxSalary:"",
+    		singleValue:""
+    } 
+    
+    /*
+     * low salary != 0 && max salary != 0 -> close range
+     * low salary = -1 && max salary = target
+     * open range low range = value && max salary - high value - open range.
+     */
+    $scope.sendSearchData = {
+    		freeText:"",
+    		companies:"",
+    		location:"",
+    		minSalary:"",
+    		maxSalary:"",
+    }
     $scope.nojobsfound = false;
    
     //get all jobs
     $http({
-        method: 'GET',
-        url: '/jobposting/open'
+        method: 'POST',
+        url: '/jobposting/search?page='+0+'&size=5',
+        data: $scope.sendSearchData
     }).success(function(data){
-
+    	console.log(data);
         $scope.jobs = data.content;
 
     }).error(function(error){
@@ -21,11 +43,11 @@ jobPortalApp.controller('controllerViewJobs', function($scope, $state, $statePar
 
         $http({
             method: 'GET',
-            url: '/jobposting/open/'+page
+            url: '/jobposting/search?page='+page+'&size=2'
         }).success(function(data){
-
-            if(data != null && data.length > 0) {
-                $scope.jobs = data;
+        	console.log(data)
+            if(data.content !== null && data.content.length > 0) {
+                $scope.jobs = data.content;
             }else{
                 $scope.jobs = null;
             }
@@ -38,7 +60,71 @@ jobPortalApp.controller('controllerViewJobs', function($scope, $state, $statePar
 
     }
 
-    $scope.mainSearch = "";
+    
+    
+    $scope.search = function(page) {
+    	console.log($scope.salaryType)
+    	if($scope.salaryType == "singlevalue") {
+    		
+    		$scope.sendSearchData.minSalary = -1;
+    		
+    		$scope.sendSearchData.maxSalary = $scope.jobSearch.singleValue;
+    		
+    	} else if ($scope.salaryType == "salaryrange") {
+    		
+    		if(($scope.jobSearch.minSalary !== null && $scope.jobSearch.minSalary !== undefined && $scope.jobSearch.minSalary !== ""  ) && 
+    		   ($scope.jobSearch.maxSalary === null || $scope.jobSearch.maxSalary === undefined || $scope.jobSearch.maxSalary === "" ) ) {
+    			
+    				$scope.sendSearchData.minSalary = $scope.jobSearch.minSalary;
+    			
+    				$scope.sendSearchData.maxSalary = 9999999;
+    		}
+    		
+    		if(($scope.jobSearch.minSalary !== null && $scope.jobSearch.minSalary !== undefined && $scope.jobSearch.minSalary !== ""  ) && 
+    	       ($scope.jobSearch.maxSalary !== null && $scope.jobSearch.maxSalary !== undefined && $scope.jobSearch.maxSalary !== "" ) ) {
+    	    			
+    	    		$scope.sendSearchData.minSalary = $scope.jobSearch.minSalary;
+    	    			
+    	    		$scope.sendSearchData.maxSalary = $scope.jobSearch.maxSalary;
+    	    }
+    		
+    		if(($scope.jobSearch.minSalary === null || $scope.jobSearch.minSalary === undefined || $scope.jobSearch.minSalary === ""  ) && 
+    	       ($scope.jobSearch.maxSalary !== null && $scope.jobSearch.maxSalary !== undefined && $scope.jobSearch.maxSalary !== "" ) ) {
+    	    	    			
+    	    	    $scope.sendSearchData.minSalary = 0;
+    	    	    			
+    	    	    $scope.sendSearchData.maxSalary = $scope.jobSearch.maxSalary;
+    	    }
+    		
+    		if(($scope.jobSearch.minSalary === null || $scope.jobSearch.minSalary === undefined || $scope.jobSearch.minSalary === ""  ) && 
+    	       ($scope.jobSearch.maxSalary === null || $scope.jobSearch.maxSalary === undefined ||$scope.jobSearch.maxSalary === "" ) ) {
+    	    	    			
+    	    	    $scope.sendSearchData.minSalary = $scope.jobSearch.minSalary;
+    	    	    			
+    	    	    $scope.sendSearchData.maxSalary = $scope.jobSearch.maxSalary;
+    	    }
+    	}
+    	
+    	$scope.sendSearchData.companies = $scope.jobSearch.companies;
+    	$scope.sendSearchData.location = $scope.jobSearch.location;
+    	$scope.sendSearchData.freeText = $scope.jobSearch.freeText;
+    	
+    	
+    	$http({
+    		method:'POST',
+    		url:'/jobposting/search?page='+page+'&size=2',
+    		data : $scope.sendSearchData
+    	}).success(function(data){
+    		if(data.content !== null && data.content.length > 0)
+    		{
+    			$scope.jobs= data.content;
+    		}
+     	})
+    	console.log($scope.sendSearchData);
+    	
+    }
+    
+    /*$scope.mainSearch = "";
     
     function findString(obj, regexp) {
     	  let found = false;
@@ -49,9 +135,9 @@ jobPortalApp.controller('controllerViewJobs', function($scope, $state, $statePar
     	  });
 
     	  return found;
-    	}
+    	}*/
     
-    $scope.mainFilter = function(job) {
+    /*$scope.mainFilter = function(job) {
     	if($scope.mainSearch === null || $scope.mainSearch === "" || $scope.mainSearch === undefined)
     		return true;
     	var matchString = JSON.stringify(job);
@@ -62,7 +148,7 @@ jobPortalApp.controller('controllerViewJobs', function($scope, $state, $statePar
     		return findString(job, words[i]);		
     	}
     	return false;
-    }
+    }*/
 
 
     $scope.toViewJob = function(requisitionId){
